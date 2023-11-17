84e3df398114fef7edf71df64e2606bc8fc912ad
david <davidwsyarif@gmail.com>
2023-11-17 14:14:17 +0700

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import InputLabel from '@mui/material/InputLabel'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import TextField from '@mui/material/TextField'
+import React from 'react'
+
+export default function Page() {
+    return (
+        <html>
+
+        </html>
+
+    )
+}
